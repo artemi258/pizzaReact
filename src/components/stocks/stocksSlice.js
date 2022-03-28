409db8aeda307634a802cab9912f99c00120f47d
ArtemKa <you@example.com>
2022-03-28 18:05:39 +0500

    updated the header and footer

--- src/components/stocks/stocksSlice.js
+++ src/components/stocks/stocksSlice.js
@@ -11,7 +11,7 @@ export const fetchStocks = createAsyncThunk(
     'stocks/fetchStocks',
     () => {
         const { request } = useHttp();
-        return request('http://localhost:3001/stocks');
+        return request('../../JSON/product.json');
     }
 );
 
