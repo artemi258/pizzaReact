25c7a35f6664f244d1ca83d4fdc6287c905c9220
ArtemKU <you@example.com>
2022-04-27 23:58:15 +0500

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
+const router = jsonServer.router('./JSON/product.json');
 const middlewares = jsonServer.defaults({
   static: './build'
 });
