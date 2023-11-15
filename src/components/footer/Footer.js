c27818c9323fdda1072f773828bd0c1ea88f6d2b
Artemiy <artem2k17xia@yandex.ru>
2023-11-15 21:20:21 +0500

    update footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,58 +1,87 @@
-import pizza from '../../icon/pizza.png';
-import facebook from '../../icon/facebook.png';
-import instagram from '../../icon/instagram.png';
-import phone from '../../icon/phone.png';
-import nav from '../../icon/nav.png';
+import pizza from "../../icon/pizza.png";
+import facebook from "../../icon/facebook.png";
+import instagram from "../../icon/instagram.png";
+import phone from "../../icon/phone.png";
+import nav from "../../icon/nav.png";
 
-import './footer.scss';
-
+import "./footer.scss";
 
 const Footer = () => {
-    return (
-        <footer className='footer'>
-            <div className="footer__wrapper container">
-                <div className="footer__logo">
-                    <div className="footer__pizza">
-                        <div className="footer__img"><img src={pizza} alt="pizza" /></div>
-                        <span>Куда пицца</span>
-                    </div>
-                    <div className="footer__copyright">© Copyright 2021 — Куда Пицца</div>
-                </div>
-                <ul className="footer__info">
-                    <span>Куда пицца</span>
-                    <li><a href="#q">О компании</a></li>
-                    <li><a href="#q">Пользовательское соглашение</a></li>
-                    <li><a href="#q">Условия гарантии</a></li>
-                </ul>
-                <ul className="footer__help">
-                    <span>Помощь</span>
-                    <li><a href="#q">Ресторан</a></li>
-                    <li><a href="#q">Контакты</a></li>
-                    <li><a href="#q">Поддержка</a></li>
-                    <li><a href="#q">Отследить заказ</a></li>
-                </ul>
-                <div className="footer__contacts">
-                            <span>Контакты</span>
-                            <div className="footer__contacts-item">
-                                <div className="footer__contacts-icon"><img src={phone} alt="phone" /></div>
-                                <a href="tel:+79509999999" target="_blank" rel='noreferrer'>+7 (950) 999-99-99</a>
-                            </div>
-                            <div className="footer__contacts-item">
-                                <div className="footer__contacts-icon"><img src={nav} alt="nav" /></div>
-                                <address>Североуральск, ул. Ленина, д.22</address>
-                            </div>
-                            <div className="footer__contacts-item">
-                                <div className="footer__contacts-icon"><img src={facebook} alt="facebook" /></div>
-                                <a href="https://www.facebook.com" target="_blank" rel='noreferrer'>Facebok</a>
-                            </div>
-                            <div className="footer__contacts-item">
-                                <div className="footer__contacts-icon"><img src={instagram} alt="instagram" /></div>
-                                <a href="https://instagram.com/" target="_blank" rel='noreferrer'>Instagram</a>
-                            </div>
-                        </div>
+  return (
+    <footer className="footer">
+      <div className="footer__wrapper container">
+        <div className="footer__logo">
+          <div className="footer__pizza">
+            <div className="footer__img">
+              <img src={pizza} alt="pizza" />
             </div>
-        </footer>
-    )
-}
+            <span>Куда пицца</span>
+          </div>
+          <div className="footer__copyright">© Copyright 2021 — Куда Пицца</div>
+        </div>
+        <ul className="footer__info">
+          <span>Куда пицца</span>
+          <li>
+            <a href="#q">О компании</a>
+          </li>
+          <li>
+            <a href="#q">Пользовательское соглашение</a>
+          </li>
+          <li>
+            <a href="#q">Условия гарантии</a>
+          </li>
+        </ul>
+        <ul className="footer__help">
+          <span>Помощь</span>
+          <li>
+            <a href="#q">Ресторан</a>
+          </li>
+          <li>
+            <a href="#q">Контакты</a>
+          </li>
+          <li>
+            <a href="#q">Поддержка</a>
+          </li>
+          <li>
+            <a href="#q">Отследить заказ</a>
+          </li>
+        </ul>
+        <div className="footer__contacts">
+          <span>Контакты</span>
+          <div className="footer__contacts-item">
+            <div className="footer__contacts-icon">
+              <img src={phone} alt="phone" />
+            </div>
+            <a href="tel:+79509999999" target="_blank" rel="noreferrer">
+              +7 (950) 999-99-99
+            </a>
+          </div>
+          <div className="footer__contacts-item">
+            <div className="footer__contacts-icon">
+              <img src={nav} alt="nav" />
+            </div>
+            <address>Североуральск, пер Школьный, 1</address>
+          </div>
+          <div className="footer__contacts-item">
+            <div className="footer__contacts-icon">
+              <img src={facebook} alt="facebook" />
+            </div>
+            <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+              Facebok
+            </a>
+          </div>
+          <div className="footer__contacts-item">
+            <div className="footer__contacts-icon">
+              <img src={instagram} alt="instagram" />
+            </div>
+            <a href="https://instagram.com/" target="_blank" rel="noreferrer">
+              Instagram
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
